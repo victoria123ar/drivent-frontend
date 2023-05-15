8c884ef8a3de0ec5fb30251c0ff16604bd26947d
Victoria <vic_andrade_@hotmail.com>
2023-05-15 00:46:17 -0300

    ticketType error fix

--- src/pages/Dashboard/Payment/TicketType/index.js
+++ src/pages/Dashboard/Payment/TicketType/index.js
@@ -1,19 +1,35 @@
 import { Container, Text, Button } from './style';
 
-export function TicketType({ active, setActive, setInPerson, selected, setSelected, form, setForm, event }) {
+export function TicketType({
+  active,
+  setActive,
+  setInPerson,
+  selected,
+  setSelected,
+  form,
+  setForm,
+  ticketsType,
+  setTicket,
+}) {
   function inPersonChange() {
     setSelected({ inPerson: true, online: false });
     setActive({ inPersonActive: true, onlineActive: false });
     setInPerson(true);
-    setForm({ ...form, online: false, totalPrice: event.inPersonPrice });
+    const [ticket] = ticketsType.filter((e) => e.isRemote);
+    setForm({ ...form, online: false });
   }
 
   function onlineChange() {
     setSelected({ inPerson: false, online: true });
     setActive({ inPersonActive: false, onlineActive: true });
     setInPerson(false);
-    setForm({ ...form, online: true, totalPrice: event.onlinePrice });
+    setForm({ ...form, online: true });
   }
+
+  const [ticketsOnline] = ticketsType.filter((e) => e.isRemote);
+  const [ticketsInPerson] = ticketsType.filter((e) => !e.isRemote);
+  const priceOnline = ticketsOnline.price / 100;
+  const priceInPerson = ticketsInPerson.price / 100;
 
   return (
     <>
@@ -21,11 +37,11 @@ export function TicketType({ active, setActive, setInPerson, selected, setSelect
       <Container>
         <Button active={active.inPersonActive} selected={selected} onClick={inPersonChange}>
           <h5>Presencial</h5>
-          <p>R$ 250</p>
+          <p>+ {priceInPerson.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</p>
         </Button>
         <Button active={active.onlineActive} selected={selected} onClick={onlineChange}>
           <h5>Online</h5>
-          <p>R$ 100</p>
+          <p>+ {priceOnline.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</p>
         </Button>
       </Container>
     </>
